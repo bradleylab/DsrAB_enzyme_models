b7f92de73fa1c2ab0a1a2978ea4e61df8548c5cb
bradleylab <bradleylab@gmail.com>
2013-11-27 22:48:56 -0600

    update mixing

--- MixingModel.R
+++ MixingModel.R
@@ -16,7 +16,7 @@ names(MixMod)[3] <- "Rred"
 Mix.alpha = 0.98467919
 
 ##based on reactant Rayleigh
-#Mix.alpha = 0.99254063
+#Mix.alpha = 0.98478247
 
 MixMod.complete = MixMod[complete.cases(MixMod),]
 Xs <- matrix(seq(0.10,0.99,by=0.01),nrow=1)
@@ -26,6 +26,7 @@ MixMod.complete<-cbind(MixMod.complete,XsM)
 n=dim(MixMod.complete)[2]
 MixMod.complete[,4:n] = (MixMod.complete$Rred-MixMod.complete[,4:n]*MixMod.complete$Rso3*Mix.alpha)/(MixMod.complete$Rox*(1-MixMod.complete[,4:n]))
 
+#plot(Xs[1,],MixMod.complete[1,4:n], type="n", ylim=c(0.950,1.0),ylab="alpha-unk",xlab="X")
 plot(Xs[1,],MixMod.complete[1,4:n], type="n", ylim=c(0.976,1.0),ylab="alpha-unk",xlab="X")
 lines(Xs[1,],MixMod.complete[1,4:n])
 lines(Xs[1,],MixMod.complete[2,4:n])
@@ -33,8 +34,9 @@ lines(Xs[1,],MixMod.complete[3,4:n])
 lines(Xs[1,],MixMod.complete[4,4:n])
 lines(Xs[1,],MixMod.complete[5,4:n])
 lines(Xs[1,],MixMod.complete[6,4:n])
-lines(Xs[1,],MixMod.complete[7,4:n])
-lines(Xs[1,],MixMod.complete[8,4:n])
+#lines 7 and 8 excluded, because these are included in calc of alpha for f near 1
+#lines(Xs[1,],MixMod.complete[7,4:n])
+#lines(Xs[1,],MixMod.complete[8,4:n])
 lines(Xs[1,],MixMod.complete[9,4:n])
 lines(Xs[1,],MixMod.complete[10,4:n])
 lines(Xs[1,],MixMod.complete[11,4:n])
