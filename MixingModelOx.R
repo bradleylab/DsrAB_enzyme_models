c6af4a1940e7e4497fe7fe0888be4aa6c0cf12ac
bradleylab <bradleylab@gmail.com>
2013-11-30 00:14:23 -0600

    fix typo *

--- MixingModelOx.R
+++ MixingModelOx.R
@@ -26,7 +26,7 @@ XsM <- Xs[rep(1:1,11),]
 MixMod.complete<-cbind(MixMod.complete,XsM)
 
 n=dim(MixMod.complete)[2]
-MixMod.complete[,5:n] = Mix.alpha*(MixMod.complete$Rso3/MixMod.complete$Rox)*(4+(8*MixMod.complete[,5:n]-8*MixMod.complete[,5:n]*MixMod.complete$MM.j+4*MixMod.complete$MM.j-3)/(-2*MixMod.complete[,5:n]-MixMod.complete[,5:n]*MixMod.complete$MM.j+*MixMod.complete$MM.j+2))
+MixMod.complete[,5:n] = Mix.alpha*(MixMod.complete$Rso3/MixMod.complete$Rox)*(4+(8*MixMod.complete[,5:n]-8*MixMod.complete[,5:n]*MixMod.complete$MM.j+4*MixMod.complete$MM.j-3)/(-2*MixMod.complete[,5:n]-MixMod.complete[,5:n]*MixMod.complete$MM.j+MixMod.complete$MM.j+2))
 
 #plot the results
 plot(Xs[1,],MixMod.complete[1,5:n], type="n", ylim=c(0.976,1.1),ylab="alpha-unk",xlab="X", col="red")
