8304b8d5c671825b91c16013bb3121823e8d788f
bradleylab <bradleylab@gmail.com>
2013-12-11 11:07:42 -0600

    check both results
    two solutions are : + sqrt(3)*….. and -sqrt(3)*…..

--- MixingModelOx.R
+++ MixingModelOx.R
@@ -27,13 +27,13 @@ MixMod.complete<-cbind(MixMod.complete,XsM)
 
 n=dim(MixMod.complete)[2]
 MixMod.complete[,5:n] = (-5*MixMod.complete$Rox - 3*Mix.alpha*MixMod.complete$Rox*MixMod.complete$Rso3 + 10*(MixMod.complete$Rox^2)* MixMod.complete[,5:n] - 2*Mix.alpha*MixMod.complete$Rox*MixMod.complete$Rso3*MixMod.complete[,5:n] -
-5*(MixMod.complete$Rox^2) + 5 *Mix.alpha*MixMod.complete$Rox*MixMod.complete$Rso3*(MixMod.complete[,5:n]^2) - sqrt(3) * sqrt (3*(Mix.alpha^2)*(MixMod.complete$Rox^2)*(MixMod.complete$Rso3^2)+ 
+5*(MixMod.complete$Rox^2) + 5 *Mix.alpha*MixMod.complete$Rox*MixMod.complete$Rso3*(MixMod.complete[,5:n]^2) + sqrt(3) * sqrt (3*(Mix.alpha^2)*(MixMod.complete$Rox^2)*(MixMod.complete$Rso3^2)+ 
 4* (Mix.alpha^2)*(MixMod.complete$Rox^2)*(MixMod.complete$Rso3^2)*MixMod.complete[,5:n] +18*(Mix.alpha^2)*(MixMod.complete$Rox^2)*(MixMod.complete$Rso3^2)*(MixMod.complete[,5:n]^2) - 
 60 * (Mix.alpha^2)*(MixMod.complete$Rox^2)*(MixMod.complete$Rso3^2)*(MixMod.complete[,5:n]^3) + 35*(Mix.alpha^2)*(MixMod.complete$Rox^2)*(MixMod.complete$Rso3^2)*(MixMod.complete[,5:n]^4))) / 
 (5*((MixMod.complete$Rox^2) - 2*(MixMod.complete$Rox^2)*MixMod.complete[,5:n] + (MixMod.complete$Rox^2)*(MixMod.complete[,5:n]^2)))
 
 #plot the results
-plot(Xs[1,],MixMod.complete[1,5:n], type="n",ylab="alpha-unk",xlab="X", col="red")
+plot(Xs[1,],MixMod.complete[1,5:n], type="n", ylab="alpha-unk",xlab="X", col="red")
 lines(Xs[1,],MixMod.complete[1,5:n],col="green")  #col=cm.colors(9)[1]
 lines(Xs[1,],MixMod.complete[2,5:n],col="green")
 lines(Xs[1,],MixMod.complete[3,5:n],col="green")
