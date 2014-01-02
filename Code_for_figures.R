119aa0b79d37777d2a51bab308c08f655b6b3067
bradleylab <bradleylab@gmail.com>
2014-01-02 15:25:54 -0600

    grayscale finalized

--- Code_for_figures.R
+++ Code_for_figures.R
@@ -14,22 +14,19 @@ OxPlot.complete = OxPlot[complete.cases(OxPlot),]
 plot(OxPlot.sorted[,1],OxPlot.sorted[,2],ylim=c(0.490,0.525), xlim=c(1.000,1.015),xlab="alpha.ox",ylab="lambda",pch=16, col=gray(OxPlot.complete$f))
 
 
-
-
 ###Figure 2C
 ###reverse plot with grayscale
 alpha.range = seq(0.980, 1.1, length=99)
 plot(alpha.range, Xs[1,], type="n", xlim=c(0.976,1.1),xlab="alpha.secondary",ylab="X")
-lines(MixMod.complete[1,4:n],Xs[1,], col=gray(f.MM.complete[1])
-lines(MixMod.complete[2,4:n],Xs[1,], col=gray(f.MM.complete[2])
-lines(MixMod.complete[3,4:n],Xs[1,], col=gray(f.MM.complete[3])
-lines(MixMod.complete[4,4:n],Xs[1,], col=gray(f.MM.complete[4])
-lines(MixMod.complete[5,4:n],Xs[1,], col=gray(f.MM.complete[5])
-lines(MixMod.complete[6,4:n],Xs[1,], ccol=gray(f.MM.complete[6])
+lines(MixMod.complete[1,4:n],Xs[1,], col=gray(f.MM.complete[1]))
+lines(MixMod.complete[2,4:n],Xs[1,], col=gray(f.MM.complete[2]))
+lines(MixMod.complete[3,4:n],Xs[1,], col=gray(f.MM.complete[3]))
+lines(MixMod.complete[4,4:n],Xs[1,], col=gray(f.MM.complete[4]))
+lines(MixMod.complete[5,4:n],Xs[1,], col=gray(f.MM.complete[5]))
+lines(MixMod.complete[6,4:n],Xs[1,], col=gray(f.MM.complete[6]))
 #lines 7 and 8 excluded, because these are included in calc of alpha for f near 1
-#lines(MixMod.complete[7,4:n],Xs[1,], col=gray(f.MM.complete[7])
-#lines(MixMod.complete[8,4:n],Xs[1,], col=gray(f.MM.complete[8])
-lines(MixMod.complete[9,4:n],Xs[1,], col=gray(f.MM.complete[9])
-lines(MixMod.complete[10,4:n],Xs[1,], col=gray(f.MM.complete[10])
-lines(MixMod.complete[11,4:n],Xs[1,], col=gray(f.MM.complete[11])
+#lines(MixMod.complete[7,4:n],Xs[1,], col=gray(f.MM.complete[7]))
+#lines(MixMod.complete[8,4:n],Xs[1,], col=gray(f.MM.complete[8]))
+lines(MixMod.complete[9,4:n],Xs[1,], col=gray(f.MM.complete[9]))
+
 
