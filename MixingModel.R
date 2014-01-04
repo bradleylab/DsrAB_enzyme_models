5a8ad310f505f041a8dde97c83b7038a0c95e48f
bradleylab <bradleylab@gmail.com>
2014-01-04 00:10:38 -0600

    consolidate code

--- MixingModel.R
+++ MixingModel.R
@@ -31,17 +31,4 @@ n=dim(MixMod.complete)[2]
 MixMod.complete[,4:n] = (MixMod.complete$Rred-MixMod.complete[,4:n]*MixMod.complete$Rso3*Mix.alpha)/(MixMod.complete$Rox*(1-MixMod.complete[,4:n]))
 
 #plot the results
-plot(Xs[1,],MixMod.complete[1,4:n], type="n", ylim=c(0.976,1.1),ylab="alpha-unk",xlab="X")
-lines(Xs[1,],MixMod.complete[1,4:n])  #col=cm.colors(9)[1]
-lines(Xs[1,],MixMod.complete[2,4:n])
-lines(Xs[1,],MixMod.complete[3,4:n])
-lines(Xs[1,],MixMod.complete[4,4:n])
-lines(Xs[1,],MixMod.complete[5,4:n])
-lines(Xs[1,],MixMod.complete[6,4:n])
-#lines 7 and 8 excluded, because these are included in calc of alpha for f near 1
-#lines(Xs[1,],MixMod.complete[7,4:n])
-#lines(Xs[1,],MixMod.complete[8,4:n])
-lines(Xs[1,],MixMod.complete[9,4:n])
-lines(Xs[1,],MixMod.complete[10,4:n])
-lines(Xs[1,],MixMod.complete[11,4:n])
-
+# See Code_for_figures.R
